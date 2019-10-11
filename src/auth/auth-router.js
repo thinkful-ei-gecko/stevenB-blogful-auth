d231ecea82d1c5e63b4df42fba21e8a5275950b1
Steven Bull <stevenwbulljr@gmail.com>
2019-10-11 13:26:43 +0000

    add additional testing and comparePasswords method to auth-service.js

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const AuthService = require('./auth-service');
 
 const authRouter = express.Router();
 const jsonBodyParser = express.json();
@@ -16,7 +17,17 @@ authRouter
       }
     }
     
-    res.send('ok');
+    AuthService.getUserWithUserName(
+      req.app.get('db'),
+      loginUser.user_name
+    )
+      .then( dbUser => {
+        if (!dbUser) {
+          return res.status(400).json({ error: 'Incorrect user_name or password' });
+        }
+        res.send('ok');
+      })
+      .catch(next);
   });
 
 module.exports = authRouter;
--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -39,8 +39,24 @@ describe.only('Auth endpoints', () => {
         return supertest(app)
           .post('/api/auth/login')
           .send(loginAttemptBody)
-          .expect(400, { error: `Missing '${field}' in request body`})
+          .expect(400, { error: `Missing '${field}' in request body`});
       });
+    });
+
+    it(`responds 400 'invalid user_name or password' when bad user_name`, () => {
+      const userInvalidUser = { user_name: 'user-not', password: 'existy' }
+      return supertest(app)
+        .post('/api/auth/login')
+        .send(userInvalidUser)
+        .expect(400, { error: `Incorrect user_name or password` })
+    });
+
+    it(`responds 400 'invalid user_name or password' when bad password`, () => {
+      const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' }
+      return supertest(app)
+        .post('/api/auth/login')
+        .send(userInvalidPass)
+        .expect(400, { error: `Incorrect user_name or password` })
     });
   });
 });
