90b97979a3b99f6283c01d49349c55bcc93ace1b
Steven Bull <stevenwbulljr@gmail.com>
2019-10-11 12:48:43 +0000

    update tests

--- test/auth-endpoints.spec.js
+++ test/auth-endpoints.spec.js
@@ -25,8 +25,22 @@ describe.only('Auth endpoints', () => {
   describe('POST /api/auth/login', () => {
     beforeEach('insertUsers', () => helpers.seedUsers(db, testUsers));
 
-    it('has a test');
+    const requiredFields = ['user_name', 'password'];
+
+    requiredFields.forEach( field => {
+      const loginAttemptBody = {
+        user_name: testUser.user_name,
+        password: testUser.password
+      }
+
+      it(`responds with 400 required error when '${field}' is missing`, () => {
+        delete loginAttemptBody[field];
+
+        return supertest(app)
+          .post('/api/auth/login')
+          .send(loginAttemptBody)
+          .expect(400, { error: `Missing '${field}' in request body`})
+      });
+    });
   });
-
-
 });
